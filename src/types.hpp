842bf78860e8b2af13b56fb8921150a2ae84661a
bd <bd@934af43b-e33b-40b6-b89d-68a0d4edfe14>
2007-12-05 22:50:35 +0000

    This was forgotten in the previous commit.
    git-svn-id: file:///media/dimitar/efbe10b8-7c95-4e31-989d-af94633c30b4/bd/projects/SVN-repository/spasm/trunk@5 934af43b-e33b-40b6-b89d-68a0d4edfe14

---
+++ src/types.hpp
@@ -0,0 +1,17 @@
+#ifndef  TYPES_HPP
+#define  TYPES_HPP
+
+#include <stack>
+
+namespace Spasm
+{
+	typedef unsigned char byte;
+	typedef int PC_t;
+	typedef int data_t;
+	typedef std::stack<PC_t> Rstack_t;
+
+	class Spasm;
+	typedef void (Spasm::*Operation) ();
+}
+#endif   // ----- #ifndef TYPES_HPP
+
