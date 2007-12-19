f2ea57a45bd7847962e269e4f71c367615fdb3a2
bd <bd@934af43b-e33b-40b6-b89d-68a0d4edfe14>
2007-12-19 18:54:35 +0000

    Added an interface namespace.
    git-svn-id: file:///media/dimitar/efbe10b8-7c95-4e31-989d-af94633c30b4/bd/projects/SVN-repository/spasm/trunk@10 934af43b-e33b-40b6-b89d-68a0d4edfe14

--- src/spasm.hpp
+++ src/spasm.hpp
@@ -1,72 +1,15 @@
 #ifndef  SPASM_HPP
 #define  SPASM_HPP
 
-#include <iostream>
+#include "spasm_impl.hpp"
 
-#include "types.hpp"
-#include "dstack.hpp"
-#include "frames.hpp"
-
-namespace SpasmImpl 
+namespace Spasm 
 {
+	using SpasmImpl::Spasm;
+	using SpasmImpl::PC_t;
+	using SpasmImpl::byte;
+}
 
 
-	class Spasm
-	{
-		public:
-			Spasm ();
-			Spasm (PC_t, const byte *, std::istream & = std::cin,
-					std::ostream & = std::cout);
-			Spasm (const Spasm &);
-			~Spasm ();
-			Spasm & operator= (const Spasm &);
-
-			void run ();
-
-			const Dstack & get_dstack () const;
-
-		private:
-			static Operation operations[];
-			static PC_t op_size;
-			PC_t pc;
-			PC_t bc_size;
-			byte *bytecode;
-			Dstack data_stack;
-			Rstack_t return_stack;
-			FrameStack frame;
-			std::istream * istr;
-			std::ostream * ostr;
-	
-			void push ();
-			void pop ();
-			void dup ();
-
-			void print ();
-			void read ();
-
-			void plus ();
-			void minus ();
-			void multiply ();
-			void divide ();
-			void modulus ();
-
-			void gotrue ();
-			void gofalse ();
-			void go ();
-
-			void call ();
-			void ret ();
-
-			void load ();
-			void store ();
-
-			// private method for copying and deleting an object
-			void copybc (const Spasm &);
-			void deleteobj ();
-	} ;
-
-
-} // namespace SpasmImpl
 #endif   // #ifndef SPASM_HPP
-
 
---
+++ src/spasm_impl.hpp
@@ -0,0 +1,72 @@
+#ifndef  SPASM_IMPL_HPP
+#define  SPASM_IMPL_HPP
+
+#include <iostream>
+
+#include "types.hpp"
+#include "dstack.hpp"
+#include "frames.hpp"
+
+namespace SpasmImpl 
+{
+
+
+	class Spasm
+	{
+		public:
+			Spasm ();
+			Spasm (PC_t, const byte *, std::istream & = std::cin,
+					std::ostream & = std::cout);
+			Spasm (const Spasm &);
+			~Spasm ();
+			Spasm & operator= (const Spasm &);
+
+			void run ();
+
+			const Dstack & get_dstack () const;
+
+		private:
+			static Operation operations[];
+			static PC_t op_size;
+			PC_t pc;
+			PC_t bc_size;
+			byte *bytecode;
+			Dstack data_stack;
+			Rstack_t return_stack;
+			FrameStack frame;
+			std::istream * istr;
+			std::ostream * ostr;
+	
+			void push ();
+			void pop ();
+			void dup ();
+
+			void print ();
+			void read ();
+
+			void plus ();
+			void minus ();
+			void multiply ();
+			void divide ();
+			void modulus ();
+
+			void gotrue ();
+			void gofalse ();
+			void go ();
+
+			void call ();
+			void ret ();
+
+			void load ();
+			void store ();
+
+			// private method for copying and deleting an object
+			void copybc (const Spasm &);
+			void deleteobj ();
+	} ;
+
+
+} // namespace SpasmImpl
+#endif   // #ifndef SPASM_IMPL_HPP
+
+
