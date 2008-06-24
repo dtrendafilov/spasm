c3f1e572666f5b3539ed4099d3a7c374da82a4ca
bd <bd@934af43b-e33b-40b6-b89d-68a0d4edfe14>
2008-06-24 10:49:12 +0000

    Added Bytecode_Memory stream and some more methods.
    git-svn-id: file:///media/dimitar/efbe10b8-7c95-4e31-989d-af94633c30b4/bd/projects/SVN-repository/spasm/trunk@24 934af43b-e33b-40b6-b89d-68a0d4edfe14

--- src/asm/bytecode.cpp
+++ src/asm/bytecode.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "bytecode.hpp"
 
@@ -14,7 +15,7 @@ namespace SpasmImpl
 		{
 		}
 
-		void Bytecode_File::push_byte (unsigned char byte)
+		void Bytecode_File::push_byte (Bytecode_Stream::byte byte)
 		{
 			_bytecode.put (byte);
 		}
@@ -30,6 +31,30 @@ namespace SpasmImpl
 				push_byte ((number >> (i << 3)) & 0xff);
 		}
 
+
+		void Bytecode_Memory::push_byte (Bytecode_Stream::byte byte)
+		{
+			_bytecode.push_back (byte);
+		}
+
+		void Bytecode_Memory::push_opcode (Bytecode_Stream::Opcode_t opcode)
+		{
+			push_byte (opcode);
+		}
+
+		void Bytecode_Memory::push_integer (int number)
+		{
+			for (int i = sizeof (int) - 1; i >= 0; --i)
+				push_byte ((number >> (i << 3)) & 0xff);
+		}
+
+
+		void Bytecode_Memory::set_location (size_t index, size_t location)
+		{
+			int l = sizeof (size_t) - 1;
+			for (int i = l; i >= 0; --i)
+				_bytecode.at (index + l - i) = ((location >> (i << 3)) & 0xff);
+		}
 
 	}	// namespace ASM
 }	// namespace SpasmImpl
--- src/asm/bytecode.hpp
+++ src/asm/bytecode.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 namespace SpasmImpl
 {
@@ -33,8 +34,11 @@ namespace SpasmImpl
 					store
 				} ;
 
+				typedef unsigned char byte;
+
 				virtual void push_opcode (Opcode_t) = 0;
 				virtual void push_integer (int) = 0;
+				virtual void set_location (size_t, size_t) = 0;
 		} ;	// class Bytecode_Stream
 
 		class Bytecode_File : public Bytecode_Stream
@@ -47,13 +51,26 @@ namespace SpasmImpl
 
 			private:
 				Bytecode_File (const Bytecode_File &);
-				Bytecode_File operator= (const Bytecode_File &);
-				void push_byte (unsigned char);
+				Bytecode_File & operator= (const Bytecode_File &);
+				void push_byte (Bytecode_Stream::byte);
 
 				std::ofstream _bytecode;
 
 
 		} ;	// class Bytecode_File
+
+
+		class Bytecode_Memory : public Bytecode_Stream
+		{
+			public:
+				virtual void push_opcode (Opcode_t);
+				virtual void push_integer (int);
+				virtual void set_location (size_t, size_t);
+			private:
+				void push_byte (Bytecode_Stream::byte);
+
+				std::vector<Bytecode_Stream::byte> _bytecode;
+		} ;	// class Bytecode_Memory
 	}	// namespace ASM
 }	// namespace SpasmImpl
 
